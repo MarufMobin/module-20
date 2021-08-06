f2b8574f108e8f33a0e4d5d98a67ee3bdb77e389
unknown <mobinh264@gmail.com>
2021-08-06 15:59:01 +0600

    Find largest number with using a function

--- largest-element.js
+++ largest-element.js
@@ -1,4 +1,4 @@
-function largerstElement( numbers ){
+/* function largerstElement( numbers ){
 
     let largest = numbers[0];
 
@@ -18,3 +18,94 @@ const numbers = [52, 25, 48, 86, 26, 36];
 const numbersOfArray = largerstElement( numbers );
 
 console.log(numbersOfArray);
+ */
+
+
+// trying purpus test code run
+
+/* function tryToForloopTest( numbers ){
+
+    let larger = numbers[0];
+
+    for( let i = 0; i < numbers.length; i++){
+        
+        let element = numbers[i];
+
+        if(  element > larger ){
+
+            larger = element;
+        }
+    }
+
+    return larger;
+}
+
+const numbers = [ 58, 15, 46, 38, 29, 8, 18, 79 ];
+
+let result = tryToForloopTest( numbers );
+
+console.log(result); */
+
+
+
+
+/* function largestNumber( numbers ){
+
+    let larger = numbers[0];
+
+    for( let i = 0; i < numbers.length; i++){
+        
+        let element = numbers[i];
+
+        if( element > larger ){
+
+            larger = element;
+        }
+    }
+    return larger;
+}
+
+let numbers = [ 25, 35, 69, 54, 34, 35 ];
+
+let ages = [ -9, -4, -2 , -32, -56 ]
+
+// let result = largestNumber( numbers );
+
+let result = largestNumber( ages );
+
+console.log( result ); */
+
+
+/* function findOutLargeNumber( numbers ){
+
+    let larger = numbers[0];
+
+    for( let i = 0; i < numbers.length; i++){
+
+        let elment = numbers[ i ];
+        
+        if( elment > larger ){
+
+            larger = elment;
+        }
+    }
+
+    return larger;
+}
+
+
+
+// const ages = [ 25, 36, 55, 78, 18, 64 ];
+
+const numbers = [ -5, -8, -9, -7, -15, -638, -2];
+
+const result = findOutLargeNumber( numbers );
+
+console.log( result ); */
+
+
+
+
+// Using a Fanction to find out a smaller number of in a array
+
+
